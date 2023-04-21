a8bdebd68e1777b92c54023cf80355d12fc47677
mixip <radovanovicmihajlo96@gmail.com>
2023-04-21 15:46:52 +0200

    American Development team

--- src/NoviTest.java
+++ src/NoviTest.java
@@ -8,10 +8,24 @@ public class NoviTest
         System.out.println("valjdfdsfsdfsda radi");
         System.out.println("raafadfdi");
         System.out.println("radiafadsf sigurno");
-        System.out.println("dodasdfasdf);
+        System.out.println("dodasdfasdf");
     }
     @Test
     public void newTes2t()
+    {
+        System.out.println("asdffasdt");
+        System.out.println("josasdfasdfog");
+        System.out.println("afjiasdffasdj");
+    }
+    @Test
+    public void newTe2s2t()
+    {
+        System.out.println("asdffasdt");
+        System.out.println("josasdfasdfog");
+        System.out.println("afjiasdffasdj");
+    }
+    @Test
+    public void newTe24s2t()
     {
         System.out.println("asdffasdt");
         System.out.println("josasdfasdfog");
