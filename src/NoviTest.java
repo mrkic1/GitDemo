0351338aa98ba0ffca5180dd06151fe92a60dd9a
mixip <radovanovicmihajlo96@gmail.com>
2023-04-21 15:39:29 +0200

    Architect changes

---
+++ src/NoviTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class NoviTest
+{
+    @Test
+    public void setup1()
+    {
+        System.out.println("valjdfdsfsdfsda radi");
+        System.out.println("raafadfdi");
+        System.out.println("radiafadsf sigurno");
+        System.out.println("dodasdfasdf);
+    }
+    @Test
+    public void newTes2t()
+    {
+        System.out.println("asdffasdt");
+        System.out.println("josasdfasdfog");
+        System.out.println("afjiasdffasdj");
+    }
+}
